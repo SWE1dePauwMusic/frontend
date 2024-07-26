3216e716fb0c27b048a8f170ffc4c9658f6090f1
Minh Nguyen <minhnguyen150403@gmail.com>
2024-07-26 11:52:02 +0700

    Fix play button

--- src/Services/playTrack.js
+++ src/Services/playTrack.js
@@ -5,27 +5,39 @@ import makeRequest from "../utils/request";
 import {getTokenHandler} from "../utils/tokenHandling";
 
 async function reqPlayTrackWithId(accessToken, deviceId, trackUris){
-    //use makeRequest
-    const options = {
+    // //use makeRequest
+    // const options = {
+    //     method: 'PUT',
+    //     url: `https://api.spotify.com/v1/me/player/play?device_id=${deviceId}`,
+    //     headers: {
+    //         'Authorization': `Bearer ${accessToken}`,
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({
+    //         uris: trackUris.map(trackUri => 'spotify:track:' + trackUri),
+    //     })
+    // }
+    // try {
+    //     const response = await makeRequest(options);
+    //     console.log("Request Play", response)
+    //     return response;
+    // } catch (error) {
+    //     console.error('Error playing track:', error);
+    //     throw error;
+    // }
+
+    await fetch(`https://api.spotify.com/v1/me/player/play?device_id=${deviceId}`, {
         method: 'PUT',
-        url: `https://api.spotify.com/v1/me/player/play?device_id=${deviceId}`,
         headers: {
             'Authorization': `Bearer ${accessToken}`,
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
             uris: trackUris.map(trackUri => 'spotify:track:' + trackUri),
-        })
-    }
-    try {
-        const response = await makeRequest(options);
-        console.log("Request Play", response)
-        return response;
-    } catch (error) {
-        console.error('Error playing track:', error);
-        throw error;
-    }
+        }),
+    }).catch(error => console.error('Error playing playlist:', error));
 }
+
 
 async function reqPlayPlaylistWithId(accessToken, deviceId, playlistUri) {
     // const accessToken = getTokenHandler("accessToken");
